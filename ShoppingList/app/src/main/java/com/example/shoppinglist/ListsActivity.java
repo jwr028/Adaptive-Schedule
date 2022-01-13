767201ea588530326d593df711e41a842a8b1fa6
Foxxybb <62300241+Foxxybb@users.noreply.github.com>
2022-01-13 14:32:43 -0600

    Update ListsActivity.java

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ListsActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ListsActivity.java
@@ -3,6 +3,9 @@ package com.example.shoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class ListsActivity extends AppCompatActivity {
 
