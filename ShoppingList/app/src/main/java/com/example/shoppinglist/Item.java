4502c5c1a3bc29393aa930a1824e047ee1b7cca3
KingCong180 <dja021@latech.edu>
2022-01-19 11:07:34 -0600

    Added some classes

---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/Item.java
@@ -0,0 +1,21 @@
+package com.example.shoppinglist;
+
+public class Item {
+    int quantity;
+    String name;
+
+    public void newItem(){
+        quantity = 1;
+        name = "Unnamed";
+    }
+
+    public void addQuantity(){
+
+    }
+
+    public void subQuantity(){
+
+    }
+
+
+}
---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/List.java
@@ -0,0 +1,21 @@
+package com.example.shoppinglist;
+
+public class List {
+    private String name;
+
+    public void newList() {
+        name = "Unnamed";
+    }
+    public void rename(){
+
+    }
+
+    public void addItem(String itemName){
+        
+    }
+
+    public void removeItem(String itemName){
+
+    }
+
+}
