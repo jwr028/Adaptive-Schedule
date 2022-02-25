a6f2b207f7e66c21b17e5cc18bf4fdbafa48b238
KingCong180 <dja021@latech.edu>
2022-02-25 13:18:02 -0600

    attempt to scrape image

--- ShoppingList/app/src/main/java/com/example/shoppinglist/WebScrape.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/WebScrape.java
@@ -1,9 +1,11 @@
 package com.example.shoppinglist;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +13,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class WebScrape extends AppCompatActivity {
 
-    TextView webTest; // Declaration of text box (if text box is changed/renamed change here)
+    TextView webText; // Declaration of text box (if text box is changed/renamed change here)
+    ImageView webImage;
     String item = "milk";
     int page = 1;
     private Button nextPage;
     private Button previousPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_scrape_specify);
 
-        webTest = findViewById(R.id.webTest);
+        webText = findViewById(R.id.webText);
+        webImage = findViewById(R.id.webImage);
         nextPage = findViewById(R.id.nextPage);
         previousPage = findViewById(R.id.previousPage);
 
@@ -55,8 +61,9 @@ public class WebScrape extends AppCompatActivity {
         });
 
     }
-
+    String thePicture;
     String theDescription;
+
     private class description_webscrape extends AsyncTask<Void, Void, Void> {
 
         @Override
@@ -67,7 +74,27 @@ public class WebScrape extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
 
-            theDescription = Scrape();
+            org.jsoup.nodes.Document document = null;
+            String url = null;
+            if (page == 1){
+                url = String.format("https://www.walmart.com/search?q=%s&affinityOverride=store_led",item);
+            } else {
+                url = String.format("https://www.walmart.com/search?q=%s&affinityOverride=store_led&page=%d",item, page);
+            }
+
+            try {
+                document = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            org.jsoup.select.Elements elementsText = document.getElementsByClass("f6 f5-l normal dark-gray mb0 mt1 lh-title");
+            org.jsoup.select.Elements elementsImage = document.select("img[class=absolute top-0 left-0]");
+
+            for (int i = 0; i<10; i++) {
+                theDescription = elementsText.eq(0).text();
+                thePicture = elementsImage.eq(0).attr("src_set");
+            }
+
 
 
             return null;
@@ -76,14 +103,14 @@ public class WebScrape extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
 
-            webTest.setText(theDescription);
-
+            webText.setText(theDescription);
+            webImage.setImageURI(Uri.parse(thePicture));
         }
 
 
     }
 
-    public String Scrape(){
+    /*public String Scrape(){
         org.jsoup.nodes.Document document = null;
         String url = null;
         if (page == 1){
@@ -97,9 +124,10 @@ public class WebScrape extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        org.jsoup.select.Elements elements = document.getElementsByClass("f6 f5-l normal dark-gray mb0 mt1 lh-title");
-        return elements.text();
-    }
+        org.jsoup.select.Elements elementsText = document.getElementsByClass("f6 f5-l normal dark-gray mb0 mt1 lh-title");
+        org.jsoup.select.Elements elementsImage = document.getElementsByClass("absolute top-0 left-0");
+        return elementsText.text();
+    }*/
 
 }
 
