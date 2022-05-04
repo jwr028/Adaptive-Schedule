9f298a40a820e7e4b35aa6641059cd25f6ff4bc0
DAIYMO <93620661+DAIYMO@users.noreply.github.com>
2022-05-04 11:46:17 -0500

    Added forgot password

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ListsActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ListsActivity.java
@@ -30,6 +30,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.Collections;
 import java.util.List;
--- ShoppingList/app/src/main/java/com/example/shoppinglist/UserProfile.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/UserProfile.java
@@ -1,18 +1,35 @@
 package com.example.shoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 public class UserProfile extends AppCompatActivity {
     TextView firstName, lastName, email, comments;
+    Button signOut, backToLists;
     private FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    GoogleSignInClient signInClient;
+    private static final String TAG = "UserProfile";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +41,52 @@ public class UserProfile extends AppCompatActivity {
         email = findViewById(R.id.display_user_email);
         comments = findViewById(R.id.comments);
 
-        
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        String userID = fAuth.getCurrentUser().getUid();
+        fStore.collection("users").document(userID).get().addOnCompleteListener(task -> {
+           if(task.isSuccessful()){
+               DocumentSnapshot userSnapshot = task.getResult();
+               if(userSnapshot.exists()){
+                   String userFirstName =  userSnapshot.get("firstName").toString();
+                   firstName.setText(userFirstName);
+                   lastName.setText(userSnapshot.get("lastName").toString());
+                   email.setText(userSnapshot.get("email").toString());
+               }
+           }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                completeSignOut();
+            }
+        });
+
+        backToLists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ListsActivity.class));
+            }
+        });
+    }
+
+    // Creating the sign out for all Accounts.
+    private void completeSignOut(){
+        // Firebase signout
+        fAuth.getInstance().signOut();
+
+        //Google client sign out
+        signInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(UserProfile.this, "You have been signed out!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), Login.class));
+                } else {
+                    Log.w(TAG, "Error signing out!");
+                }
+            }
+        });
     }
 }
